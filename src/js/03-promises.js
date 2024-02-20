3bb63eaba71f9b5b5f81c1656f0206f267e12a73
AWolewicz <agnieszkawolewicz5@gmail.com>
2024-02-20 09:30:58 +0100

    task 2 & 3

---
+++ src/js/03-promises.js
@@ -0,0 +1,32 @@
+const ourForm = document.querySelector('.form');
+
+ourForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const delay = Number(e.target.elements[0].value);
+  const step = Number(e.target.elements[1].value);
+  const amount = Number(e.target.element[2].value);
+
+  for (let i = 0; 1 < amount; i++) {
+    createPromise(i, delay + i * step)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position + 1} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position + 1} in ${delay}ms`);
+      });
+  }
+});
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  })
+}
